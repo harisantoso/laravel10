25767d7704ea935706eff508c1e0ef593c5df1d5
harisantoso <harysanttos@gmail.com>
2024-06-21 04:54:16 +0700

    add blog api

---
+++ blog-app-backend/app/Http/Controllers/BlogController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BlogController extends Controller
+{
+    // this method will return all blogs
+    public function index()
+    {
+    }
+
+    // this method will return a single blog
+    public function show()
+    {
+    }
+
+    // this method will store a blog
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|min:10',
+            'author' => 'required|min:3'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'please fix the errors',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $blog = new Blog();
+        $blog->title = $request->title;
+        $blog->author = $request->author;
+        $blog->description = $request->description;
+        $blog->shortDesc = $request->shortDesc;
+        $blog->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'blog added successfully',
+            'data' => $blog
+        ]);
+    }
+
+    // this method will update a blog
+    public function update()
+    {
+    }
+
+    // this method will delete a blog
+    public function destroy()
+    {
+    }
+}
